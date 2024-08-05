3d444cbe45df7edf34e0a301129b301222e9bac9
BilalArif <bilalarif093@gmail.com>
2024-08-05 13:37:45 +0500

    GUI

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import javax.swing.*;
 import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) {
+//        JOptionPane.showMessageDialog(null,"hello");
+//        String num = JOptionPane.showInputDialog("Enter your number");
+        String num1 = JOptionPane.showInputDialog(null,"Enter your number","Input dialog",0);
+        System.out.println(num1);
+        //code + drag and drop
+
+
+
+
+
+
+
+
+
+
+
         //declare
         int [] arr = new int[10];//dynamic array
         int [] arr1 = {70,100,10,5};//static array
@@ -10,16 +27,16 @@ public class Main{
 //            System.out.println(arr1[i]);
 //        }
         //foreach loop
-        int index = 0;
-        for (int data : arr1){
-            System.out.println(data);
-            index++;
-        }
-        Arrays.sort(arr1);
-        System.out.println(Arrays.binarySearch(arr1,100));
-        for (int data :arr1){
-            System.out.println(data);
-        }
+//        int index = 0;
+//        for (int data : arr1){
+//            System.out.println(data);
+//            index++;
+//        }
+//        Arrays.sort(arr1);
+//        System.out.println(Arrays.binarySearch(arr1,100));
+//        for (int data :arr1){
+//            System.out.println(data);
+//        }
 
     }
 }
