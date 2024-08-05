97c11008c1be53db78732fbb756a7ac66410431d
BilalArif <bilalarif093@gmail.com>
2024-08-06 00:50:40 +0500

    two recursive problems

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
 public class Main {
     public static void main(String[] args) {
+        //recursive
+        /*
+        * int Power (base, exp){}
+        * 2^4 = 2 * 2^3 = 16
+        * 2^3 = 2 * 2^2 = 8
+        * 2^2 = 2 * 2 = 2
+        * 2^1 = 2 * 1 = 2
+        * 2^0 = 1//base condition
+        *
+        * recursive problem always solve through break down process
+        * n^p = n * n^p-1 (recursive call)
+        *
+        * */
+
+        //recursion
+        /*
+        *  a function call itself
+        *  5 4 3 2 1 solution recursive code
+        *  through stack and through recursive tree
+        *
+        * */
+        //printSeries(5);
+        //remaining code
+        System.out.println("Power = "+power(2,4));
+        //Power = 16
 
     }
+    //Power function using recursion
+    static int power (int base, int exp){//base= 2, exp = 0
+        if(exp==0) {//exp == 0 true
+            return 1;
+        }
+        else {
+            return base * power(base, exp - 1);//exp 1-1 = 0
+        }
+    }
+
+
+
+
+
+
+
+//    //infinite time recursive calls
+    public static void printSeries(int num){//1
+        if(num>1) {//false
+            printSeries(num - 1);
+        }
+        else {
+            System.out.println(num);//1 2 3 4 5
+        }
+        //return
+    }
+
 }
